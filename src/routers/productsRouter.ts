36820a544bd67812761f5def71dd74491aabd6e7
marcojr73 <marcojuniorengineer@gmail.com>
2022-11-01 08:58:05 -0300

    update product and validation body

--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import * as controller from "../controllers/productsController.js"
+import validateDataProduct from "../middlewares/validateDataProduct.js"
 
 const productsRouter = Router()
 
 productsRouter.get("/", controller.apiDetails)
-productsRouter.put("/products/:code", controller.productUpdate)
+productsRouter.put("/products/:code", validateDataProduct, controller.updateProduct)
+productsRouter.delete("/products/:code", controller.toTrashProduct)
 productsRouter.get("/products/:code", controller.getProductInfo)
 
 export default productsRouter
